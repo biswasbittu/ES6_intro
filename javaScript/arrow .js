b18ffcc5ab94fa2cc0666216b226fce2e1c5ea41
biswasbittu <biswasbittu7415@yahoo.com>
2023-02-20 16:11:11 +0530

    arrow function decleration

---
+++ javaScript/arrow .js
@@ -0,0 +1,19 @@
+// function Decleration............
+// function add(first,second){
+//     const total=first+second;
+//     return total;
+// }
+
+// const result=add(10,20);
+// console.log(result);
+
+// Another Decleration of Function........
+
+// const add =(num1,num2) => num1 + num2;
+
+// const result=add(5,6);
+// console.log(result);
+ const add =(num1,num2)=> num1+num2;
+ const result=add(10,20);
+ console.log(result);
+ 
---
+++ javaScript/dom.js
@@ -0,0 +1,13 @@
+document.getElementById('apply-bg').addEventListener('click',function(){
+    const friends=document.getElementsByClassName('friend');
+    for(const friend of friends){
+    friend.style.backgroundColor='red';
+
+
+    }
+})
+
+document.getElementById('center-third').addEventListener('click',function(){
+    const third=document.getElementById('third-friend');
+    third.style.textAlign='center'
+})
